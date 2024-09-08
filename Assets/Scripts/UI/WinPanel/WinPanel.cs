8ca3491f0e1daf58e5a9dd065ce0bce9c6c12d9d
ramannamar <145287539+ramannamar@users.noreply.github.com>
2024-09-08 05:38:21 +0300

    Update WinPanel.cs

--- Assets/Scripts/UI/WinPanel/WinPanel.cs
+++ Assets/Scripts/UI/WinPanel/WinPanel.cs
@@ -6,88 +6,88 @@ using Enviroment;
 
 namespace UI.WinPanel
 {
-    public class WinPanel : MonoBehaviour
-    {
-        [SerializeField] private Button restartButton;
-        [SerializeField] private Button quitButton;
-        [SerializeField] private RectTransform winPanelRectTransform;
-        [SerializeField] private GameOverPanel gameOverPanel;
-        [SerializeField] private float animationDuration = 1.0f;
-        [SerializeField] private WinPanelStarsAnimation winPanelStarsAnimation;
+	public class WinPanel : MonoBehaviour
+	{
+		[SerializeField] private Button restartButton;
+		[SerializeField] private Button quitButton;
+		[SerializeField] private RectTransform winPanelRectTransform;
+		[SerializeField] private GameOverPanel gameOverPanel;
+		[SerializeField] private float animationDuration = 1.0f;
+		[SerializeField] private WinPanelStarsAnimation winPanelStarsAnimation;
 
-        private int collectedStars = 0;
+		private int collectedStars = 0;
 
-        void Start()
-        {
-            InitializeButtons();
-    
-            gameOverPanel.OnGameOver.AddListener(OnGameOver);  
-            winPanelRectTransform.anchoredPosition = new Vector2(0, Screen.height);
-        }
+		private void OnEnable()
+		{
+			StarsCollect.starCollected += OnStarCollected;
+		}
+		
+		void Start()
+		{
+			InitializeButtons();
+	
+			gameOverPanel.OnGameOver.AddListener(OnGameOver);  
+			winPanelRectTransform.anchoredPosition = new Vector2(0, Screen.height);
+		}
 
-        private void OnEnable()
-        {
-            StarsCollect.starCollected += OnStarCollected;
-        }
+		private void OnDisable()
+		{
+			StarsCollect.starCollected -= OnStarCollected;
+		}
 
-        private void OnDisable()
-        {
-            StarsCollect.starCollected -= OnStarCollected;
-        }
+		private void InitializeButtons()
+		{
+			restartButton.onClick.AddListener(RestartButton.LevelRestart);
+			quitButton.onClick.AddListener(QuitButton.QuitGame);
+		}
 
-        private void InitializeButtons()
-        {
-            restartButton.onClick.AddListener(RestartButton.LevelRestart);
-            quitButton.onClick.AddListener(QuitButton.QuitGame);
-        }
+		private void OnStarCollected(GameObject star, bool collected)
+		{
+			if (collected)
+				collectedStars++;
+		}
 
-        private void OnStarCollected(GameObject star, bool collected)
-        {
-            if (collected)
-                collectedStars++;
-        }
+		private void OnGameOver()
+		{
+			StartCoroutine(ShowWinPanel());
+		}
 
-        private void OnGameOver()
-        {
-            StartCoroutine(ShowWinPanel());
-        }
+		private IEnumerator ShowWinPanel()
+		{
+			float elapsedTime = 0;
 
-        private IEnumerator ShowWinPanel()
-        {
-            float elapsedTime = 0;
+			Vector2 startPosition = winPanelRectTransform.anchoredPosition;
+			Vector2 endPosition = Vector2.zero;
 
-            Vector2 startPosition = winPanelRectTransform.anchoredPosition;
-            Vector2 endPosition = Vector2.zero;
+			while (elapsedTime < animationDuration)
+			{
+				winPanelRectTransform.anchoredPosition = Vector2.Lerp(startPosition, endPosition, (elapsedTime / animationDuration));
+				elapsedTime += Time.deltaTime;
+				yield return null;
+			}
 
-            while (elapsedTime < animationDuration)
-            {
-                winPanelRectTransform.anchoredPosition = Vector2.Lerp(startPosition, endPosition, (elapsedTime / animationDuration));
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
+			winPanelRectTransform.anchoredPosition = endPosition;
 
-            winPanelRectTransform.anchoredPosition = endPosition;
+			StartCoroutine(ActivateStars());
+		}
 
-            StartCoroutine(ActivateStars());
-        }
+		private IEnumerator ActivateStars()
+		{
+			int starsToActivate = 0;
+			if (collectedStars >= 10)
+			{
+				starsToActivate = 3;
+			}
+			else if (collectedStars >= 4)
+			{
+				starsToActivate = 2;
+			}
+			else if (collectedStars >= 3)
+			{
+				starsToActivate = 1;
+			}
 
-        private IEnumerator ActivateStars()
-        {
-            int starsToActivate = 0;
-            if (collectedStars >= 10)
-            {
-                starsToActivate = 3;
-            }
-            else if (collectedStars >= 4)
-            {
-                starsToActivate = 2;
-            }
-            else if (collectedStars >= 3)
-            {
-                starsToActivate = 1;
-            }
-
-            yield return winPanelStarsAnimation.ActivateStarsWithAnimation(starsToActivate);
-        }
-    }
+			yield return winPanelStarsAnimation.ActivateStarsWithAnimation(starsToActivate);
+		}
+	}
 }
